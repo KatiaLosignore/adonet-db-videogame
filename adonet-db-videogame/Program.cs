a17aae56425c87f4370b68e88d89a54862e9670a
KatiaLosignore <katialos.kl@gmail.com>
2023-09-21 15:26:57 +0200

    added class Videogame

--- adonet-db-videogame/Program.cs
+++ adonet-db-videogame/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Benvenuto nel nostro sistema di gestione Videogiochi!");
+
+
+while (true)
+{
+    Console.WriteLine(@"
+- 1: Inserisci un nuovo videogioco 
+- 2: Ricerca un videogioco per id
+- 3: Ricerca tutti i videogiochi aventi il nome contenente una determinata stringa inserita in input
+- 4: Cancella un videogioco
+- 5: Chiudi il programma
+");
+
+    Console.Write("Seleziona l'opzione desiderata: ");
+
+    int selectOption = int.Parse(Console.ReadLine());
+
+    switch (selectOption)
+    {
+        case 1:
+
+            break;
+    }
+
+
+
+
+
+
+}
---
+++ adonet-db-videogame/Videogame.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adonet_db_videogame
+{
+    public class Videogame
+    {
+        // ATTRIBUTI
+        public long Id { get; private set; }
+
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException(null, "Il campo \"Nome\" non può essere vuoto!");
+                }
+                else
+                {
+                    this.name = value;
+                }
+
+            }
+        }
+
+        private string overview;
+        public string Overview
+        {
+            get
+            {
+                return this.overview;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException(null, "Il campo \"Descrizione\" non può essere vuoto!");
+                }
+                else
+                {
+                    this.overview = value;
+                }
+
+            }
+        }
+            
+        public DateTime ReleaseDate { get; private set; }
+        public long SoftwareHouseId {  get; private set; }
+
+        // COSTRUTTORE
+        public Videogame(long id, string name, string overview, DateTime releaseDate, long softwarehouse_id)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Overview = overview;
+            this.ReleaseDate = releaseDate;
+            this.SoftwareHouseId = softwarehouse_id;
+        }
+
+        // METODI
+        public override string ToString()
+        {
+            return $"ID: {Id} Nome: {Name} Descrizione: {Overview} Data di rilascio: {ReleaseDate.ToString("dd/MM/yyyy")}";
+        }
+
+    }
+}
